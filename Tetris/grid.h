6c6af2d6ead8d07465b12d00cb5bacba7455e06d
scaverly <scaverlyy@gmail.com>
2026-03-08 08:57:22 +0500

    Creating The Grid
    Add grid.h
    Add grid.cpp

---
+++ Tetris/grid.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <raylib.h>
+
+class Grid {
+public:
+	Grid();
+	void Initialize();
+	void Print();
+	void Draw();
+	int grid[20][10];
+private:
+	std::vector<Color> GetCellColor();
+	int numRows;
+	int numCols;
+	int cellSize;
+	std::vector<Color> colors;
+};
--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,14 +1,22 @@
 ﻿#include <raylib.h>
+#include "grid.h"
 
 int main() {
 	Color darkBlue = { 44, 44, 127, 255 };
 	InitWindow(300, 600, "Tetris");
 	SetTargetFPS(60);
+
+	Grid grid = Grid();
+	grid.grid[0][0] = 3;
+	grid.Print();
+
 	while (WindowShouldClose() == false) {
 		BeginDrawing();
 		ClearBackground(darkBlue);
+
+		grid.Draw();
+
 		EndDrawing();
 	}
-
 	CloseWindow();
 }
