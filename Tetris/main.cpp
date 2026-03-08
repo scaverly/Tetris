9749d89d404b2b89e1eb85d90c98ce4966d998db
scaverly <scaverlyy@gmail.com>
2026-03-08 07:12:40 +0500

    Game The Loop

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,5 +1,14 @@
 ﻿#include <raylib.h>
 
 int main() {
-	
+	Color darkBlue = { 44, 44, 127, 255 };
+	InitWindow(300, 600, "Tetris");
+	SetTargetFPS(60);
+	while (WindowShouldClose() == false) {
+		BeginDrawing();
+		ClearBackground(darkBlue);
+		EndDrawing();
+	}
+
+	CloseWindow();
 }
